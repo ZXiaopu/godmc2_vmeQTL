3b707306fc739c974a22d9e949e23abfacbc7066
Xiaopu Zhang <k20125144@erc-hpc-login2.create.kcl.ac.uk>
2026-09-21 14:41:05 +0100

    remove old folder

--- 04_meta_QC/02a_submit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-#SBATCH --job-name=GoDMC_02a
-#SBATCH --output=job_reports/GoDMC_02a_%j
-#SBATCH --partition interruptible_cpu,gpu,cpu
-#SBATCH --mem=64GB
-#SBATCH --ntasks=8
-#SBATCH --time=12:0:0
-
-bash 02a_LDclumping_cojo.sh ${SLURM_ARRAY_TASK_ID} ${1}
--- 04_meta_QC/godmc_stage2_02b.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-#SBATCH --job-name=GoDMC_02b
-#SBATCH --output=job_reports/GoDMC_02b_%j
-#SBATCH --partition interruptible_cpu,gpu,cpu
-#SBATCH --mem=64GB
-#SBATCH --ntasks=8
-#SBATCH --time=12:0:0
-
-bash 02b_combine_LD_cojo_res.sh ${SLURM_ARRAY_TASK_ID}
